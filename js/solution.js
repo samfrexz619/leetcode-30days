22095f3accc0fda77c62152a6d0d70c5dca7aa4a
samfrexz619 <samfrexz69@gmail.com>
2024-01-19 10:33:06 +0100

    add reducer func

--- js/solution.js
+++ js/solution.js
@@ -137,3 +137,39 @@ class filtered {
 const arr = [3, 4, 50, 90]
 const newArr = new filtered()
 console.log(newArr.filter(arr, newArr.fn))
+
+/**
+ * Given an integer array nums, a reducer function fn, and an initial value init, return the final result obtained by executing the fn function on each element of the array, sequentially, passing in the return value from the calculation on the preceding element.
+
+    This result is achieved through the following operations: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The ultimate value of val is then returned.
+
+  If the length of the array is 0, the function should return init.
+
+  Please solve it without using the built-in Array.reduce method.
+ */
+
+// Array Reduce Transformation
+class Reduce {
+
+  sum = (accum, curr) => {
+    return accum + curr;
+  }
+
+  fn = this.sum;
+
+  reduce =(nums, fn, init) => {
+    let res = init;
+    for(let i = 0; i < nums.length; i++){
+      if(nums.length > 0){
+        res = fn(res, nums[i])
+      } else {
+        return res;
+      }
+    }
+    return res;
+  }
+} 
+const reduce = new Reduce()
+const nums = [1]
+const init = 0;
+console.log(reduce.reduce(nums, reduce.fn, init))
